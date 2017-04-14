8232d9945ce241927bcc220c2eb560997e51eb49
Nigel Orion <nigelorion@gmail.com>
2017-04-14 15:19:08 -0700

    add nav functions, and repair mobile class bug

--- js/script.js
+++ js/script.js
@@ -1,18 +1,39 @@
 $(document).on('click', 'a', function(event){
     event.preventDefault();
 
+    // $('#nav').toggleClass('mobileNav');
+
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 450);
 
 
+    if ($('#nav').hasClass('mobileNav')){
+      $('#nav').toggleClass('mobileNav');
+      $('.links').toggleClass('show');
+      $('.off').toggle();
+      $('.on').toggle();
+    };
+
+
 
 });
 
-$('#nav-toggle').click(function(){
-  $(this).toggleClass("on");
-  $('#nav').toggleClass('mobileNav');
-  $('.links').css({ opacity: 1 });
+$('.hamburger').click(function() {
+	$('#nav').toggleClass('mobileNav');
+	$('.links').toggleClass('show');
+
+	$('.off').toggle();
+	$('.on').toggle();
 
 
 });
+
+// $('#nav-toggle').click(function(){
+//   $(this).toggleClass("on");
+//   $('#nav').toggleClass('mobileNav');
+//   $('.links').toggleClass('off');
+//   // $('.links').css({ opacity: 1 });
+//
+//
+// });
